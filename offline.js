97caf408f0a242090cc4bca5fddd788f68c91010
semicon <1210@sts.ac.th>
2020-04-29 15:59:36 +0700

    growbutton
    growbutton

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1588150667,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bgsetting-sheet0.png",
+		"images/buttons-sheet0.png",
+		"images/buttons-sheet1.png",
+		"images/textname-sheet0.png",
+		"images/bg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
